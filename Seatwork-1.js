5861649c20cf729f90310146a06ea2b0500c98d4
Caster Troy Almeda <95603-18531452@SERVER2.DOMAIN>
2024-08-19 18:27:09 +0800

    First Commit

---
+++ Seatwork-1.js
@@ -0,0 +1,13 @@
+const user = {
+    fname: "Caster Troy",
+    lname: "Almeda",
+    gender: "Male",
+    age: 24
+}
+
+value1 = user.fname
+value2 = user.lname
+value3 = user.gender
+value4 = user.age
+
+console.log("I am " + " " + value1 + " " + value2 + " " + " and I am " + value4 + " " + " years old!");
